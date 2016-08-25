f87cfc094ea974e939eeaef0a80acfe83ca0c268
ztktct <286310437@qq.com>
2016-08-25 22:48:02 +0800

    修复头部不能收藏的错误

--- jdly/src/components/header.vue
+++ jdly/src/components/header.vue
@@ -3,7 +3,7 @@
 		<i class="icon-back" @click="goback"></i>
 		<h2>{{isContent == 1 ? currentPage.title : '我的收藏'}}</h2>
 		<!-- 如果是详情页则显示,0为不显示，1为详情页,显示 -->
-		<i class="icon-collection white" :class="{checked:currentPage.collected}" v-if="isContent == 1"></i>
+		<i class="icon-collection white" @click="addAndRemoveCollect(currentPage)" :class="{checked:currentPage.collected}" v-if="isContent == 1"></i>
 	</header>
 </template>
 
@@ -12,11 +12,6 @@
 	import {toggleContent,toggleCollections,addAndRemoveCollect} from '../vuex/action.js';
 
 	export default{
-		data(){
-			return{
-
-			}
-		},
 		props:['isContent'],
 		methods:{
 			// 返回上一页
